0db1270428099263991be811d913606e5393fd2a
TAK <infernal-slam@hotmail.com>
2016-08-04 17:38:30 +0700

    insert javascript

---
+++ public/app.js
@@ -0,0 +1,21 @@
+/*global angular*/
+angular.module('todoApp', [])
+  .controller('TodoListController', function ($scope, $http) {
+    // $(function () {
+    //   var obj = $('.DragArea')
+    //   obj.on('dragover', function (e) {
+    //     e.stopPropagation()
+    //     e.preventDefault()
+    //     $(this).css('border-color', "#c37b7b")
+    //   })
+    //   obj.on('drop', function (e) {
+    //     e.stopPropagation()
+    //     e.preventDefault()
+    //     $(this).css('border-color', "#c37b7b")
+    //     var files = e.originalEvent.dataTransfer.files
+    //     console.log(files)
+    //     var file = files[0]
+    //     $scope.file_Detail = files[0]
+    //   })
+    // })
+  })
---
+++ public/mycss.js
@@ -0,0 +1,71 @@
+import React, {StyleSheet, Dimensions, PixelRatio} from "react-native";
+const {width, height, scale} = Dimensions.get("window"),
+    vw = width / 100,
+    vh = height / 100,
+    vmin = Math.min(vw, vh),
+    vmax = Math.max(vw, vh);
+
+export default StyleSheet.create({
+    "DragArea": {
+        "backgroundColor": "white",
+        "borderStyle": "dashed",
+        "borderColor": "#dadfe3",
+        "borderRadius": 5
+    },
+    "title": {
+        "color": "#8e99a5",
+        "fontWeight": "bold",
+        "fontSize": 164,
+        "textAlign": "center"
+    },
+    "title2": {
+        "textAlign": "center",
+        "color": "#8e99a5",
+        "fontWeight": "bold",
+        "fontSize": 25
+    },
+    "link-skin": {
+        "cursor": "pointer",
+        "backgroundColor": "#0099ff",
+        "fontWeight": "bold",
+        "fontSize": 18,
+        "textDecoration": "none",
+        "textAlign": "center",
+        "borderBottom": "9px #006bb3 solid",
+        "borderRadius": 20,
+        "width": "100%",
+        "paddingTop": 10,
+        "paddingRight": 10,
+        "paddingBottom": 10,
+        "paddingLeft": 10
+    },
+    "title3": {
+        "color": "#fff",
+        "textAlign": "center"
+    },
+    "fileUpload": {
+        "position": "relative",
+        "overflow": "hidden",
+        "marginTop": 10,
+        "marginRight": 10,
+        "marginBottom": 10,
+        "marginLeft": 10
+    },
+    "fileUpload inputupload": {
+        "position": "absolute",
+        "top": 0,
+        "right": 0,
+        "marginTop": 0,
+        "marginRight": 0,
+        "marginBottom": 0,
+        "marginLeft": 0,
+        "paddingTop": 0,
+        "paddingRight": 0,
+        "paddingBottom": 0,
+        "paddingLeft": 0,
+        "fontSize": 20,
+        "cursor": "pointer",
+        "opacity": 0,
+        "filter": "alpha(opacity=0)"
+    }
+});
